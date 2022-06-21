02f94730554c8b0863d93ff39c99fda2b3c8910c
Bolanos-kun <106789261+Bolanos-kun@users.noreply.github.com>
2022-06-21 11:19:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal();
+
+sr.reveal(`.arriba`, {
+    duration:1000,
+    origin:`bottom`,
+    distance: `-100px`
+});
+sr.reveal(`.izquierda`, {
+    duration:1000,
+    origin:`rigth`,
+    distance: `-100px`
+});
+sr.reveal(`.derecha`, {
+    duration:1000,
+    origin:`left`,
+    distance: `-100px`
+});
+sr.reveal(`.abajo`, {
+    duration:1000,
+    origin:`top`,
+    distance: `-100px`
+});
